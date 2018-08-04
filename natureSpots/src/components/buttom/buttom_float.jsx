ef9816dd72018c76b441586093ec8139d4de56cb
Carlos <chnm@cin.ufpe.br>
2018-08-04 16:27:00 -0300

    Cria aplicação React-native da solução

---
+++ natureSpots/src/components/buttom/buttom_float.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Button } from 'react-native-elements';
+
+
+export default class My_buttom extends Component{
+    render(){
+        return(
+            <view>
+                <Button title='BUTTON'/>
+            </view>
+        )
+    }
+}
---
+++ natureSpots/src/layouts/home/App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import My_list from '../../components/lista/list';
+import My_map from '../../components/gmap/map'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <My_list/>
+        <My_map/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
