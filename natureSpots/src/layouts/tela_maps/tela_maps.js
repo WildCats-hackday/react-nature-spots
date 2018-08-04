3e6478caaffea78e65effe5a2c85896a86e88d85
Carlos <chnm@cin.ufpe.br>
2018-08-04 20:33:12 -0300

    GoogleMaps funcional

--- natureSpots/src/layouts/tela_maps/tela_maps.js
+++ natureSpots/src/layouts/tela_maps/tela_maps.js
@@ -1,10 +1,33 @@
 import {StyleSheet, Text, View, FlatList, Image, Alert, TouchableOpacity } from 'react-native';
 import React, {Component} from 'react';
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
+   
 
 export default class Tela_maps extends Component{
     render(){
         return(
-            <View>
+            <View style ={styles.container}>
+                <MapView style={styles.map}
+                    initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                    }}
+                />
             </View>
         );
     }
